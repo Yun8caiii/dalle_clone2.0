0edc17767058c01c03d7ff04051a4e8d7236f51d
Yun8caiii <47008027+Yun8caiii@users.noreply.github.com>
2023-01-31 18:27:46 -0800

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,12 @@
+//index js file is used to export all the components from the 
+//components folder
+
+import Card from "./Card";
+import FormField from "./FormField";
+import Loader from "./Loader";
+
+export { 
+    Card, 
+    FormField, 
+    Loader 
+};
